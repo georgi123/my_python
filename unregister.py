e4fa784347b08624896a52a12a2f116020003e22
georgi123 <gogo3888@mail.bg>
2020-01-24 12:15:17 +0200

    Update unregister.py

--- unregister.py
+++ unregister.py
@@ -3,13 +3,13 @@
 #####use the script when have to revert from sles12 to sles11#######
 import os
 ##unregister server from APX#####
-if os.path.exists("/opt/apxpccp/pccprel") || os.path.exists("/opt/apxpccc/pccCrel"):
+if os.path.exists("/opt/apxpccp/pccprel"): # os.path.exists("/opt/apxpccc/pccCrel")
     os.system('/opt/apxpccp/pccprel exec,DCM/DCM_DEREG_$(hostname).scp')
     os.system('/opt/apxpccc/pccCrel exec,DCM/DCM_DEREG_$(hostname).scp')
 if os.path.exists("/etc/machine-id"):
    os.remove("/etc/machine-id")
    os.system('dbus-uuidgen --ensure=/etc/machine-id')
 ##unregister server from HPSA##
-if os.path,exists("/opt/opsware/agent/bin/agent_uninstall.sh"):
+if os.path.exists("/opt/opsware/agent/bin/agent_uninstall.sh"):
    os.system('/opt/opsware/agent/bin/agent_uninstall.sh --no_deactiva te --force')
-   os.system('rm -r /opt/opsware');
+   os.system('rm -r /opt/opsware')
