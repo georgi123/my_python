b2dfccf0c5c374993a9f0d0236c35ee3e95829d7
georgi123 <gogo3888@mail.bg>
2020-07-14 11:21:03 +0300

    Update smtp.py
    New version

--- smtp.py
+++ smtp.py
@@ -1,3 +1,26 @@
+####New Version have to test#### 
+import smtplib
+
+sender='gdimitrov@borica.bg'
+receiver='gdimitrov@borica.bg'
+
+message='''
+From:Georgi Dimitrov
+To:gdimitrov@borica.bg
+Subject:test
+
+this a test message.
+'''
+try:
+        smtpObj = smtplib.SMTP('osvsuse121t', 25)
+        smtpObj.sendmail(sender, receiver, message)
+        print("Successfully sent email")
+except:
+         print("Error: unable to send email")
+#####end of new versioon#####
+
+
+
 import smtplib
 
 sender='gdimitrov@borica.bg'
